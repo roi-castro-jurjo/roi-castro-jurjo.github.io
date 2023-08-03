5a54db21de014ba66803e905ad02fdbe42c08b69
Roi Castro <roi.castro.jurjo@rai.usc.es>
2023-08-03 02:05:18 +0200

    Added Text Animations

--- script.js
+++ script.js
@@ -7,3 +7,53 @@ startButton.addEventListener("mouseover", () => {
   startButton.addEventListener("mouseout", () => {
     startButton.src = "assets/images/start-button-0.png";
   });
+
+
+
+
+
+
+
+function consoleText(words, ids) {
+  var currentIndex = 0; // Índice para controlar el elemento actual
+
+  function animateText() {
+    var target = document.getElementById(ids[currentIndex]);
+    var letterCount = 0;
+
+    function animateLetters() {
+      if (letterCount === words[currentIndex].length) {
+        target.innerHTML = target.innerHTML.slice(0,-1)
+        // Si ya se mostraron todas las letras, avanzamos al siguiente elemento
+        currentIndex += 1;
+        if (currentIndex < ids.length) {
+          setTimeout(animateText, 1000); // Esperamos 1 segundo antes de iniciar la siguiente animación
+        }
+        return;
+      }
+
+      target.innerHTML = words[currentIndex].substring(0, letterCount + 1) + "█";
+      letterCount += 1;
+
+      // Llamamos recursivamente la función para mostrar la siguiente letra
+      setTimeout(animateLetters, 100);
+    }
+
+    animateLetters(); // Iniciamos la animación para el elemento actual
+  }
+
+  // Iniciamos la animación sobre el primer elemento al cargar la página
+  document.addEventListener('DOMContentLoaded', function () {
+    animateText();
+  });
+
+  
+}
+
+// Uso de la función
+consoleText(
+  ['Roi Castro', 'Web developer'],
+  ['title-name', 'subtitle']
+);
+
+
