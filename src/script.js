070c60ad5fe124b72a1a096176bae352c0a1105a
roi-castro-jurjo <rcastro@trileucosolutions.com>
2024-10-01 03:39:37 +0200

    Added cube movement

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
+import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -6,34 +8,51 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
+// Axes
+const axesHelper = new THREE.AxesHelper(2)
+axesHelper.setColors('white', 'green', 'blue')
+scene.add(axesHelper)
+
+// Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Clock
+const clock = new THREE.Clock()
+
+// Animation
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    mesh.rotation.y = elapsedTime * Math.PI/2 * 0.1
+    mesh.rotation.x = elapsedTime * Math.PI/2
+    mesh.rotation.z = elapsedTime * Math.PI/2 * 0.1
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
